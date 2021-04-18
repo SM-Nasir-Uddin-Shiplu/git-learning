7528cc00a1deb1b98f1c3fa7e7c7cb9b2e9dddcc
Nasir Uddin Shiplu <nasiruddinshiplu@gmail.com>
2021-04-18 18:32:25 +0600

    Added a new method to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("VS CODE IS AWESOME");
+		greeting("Shiplu")
+	}
+
+	public static void greeting(String[] args) {
+		System.out.println("Welcome" + name);
 	}
 }
